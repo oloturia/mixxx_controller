907544309959b19a09a8c0ae42d726d2ed8fba6f
oloturia <oloturia@gmail.com>
2023-07-11 03:38:28 +0200

    First working status

--- mixxx_controller.ino
+++ mixxx_controller.ino
@@ -1,28 +1,28 @@
+#include "Arduino.h"
 #include "MIDIUSB.h"
-#include <light_CD74HC4067.h>
-
-CD74HC4067 mux(4, 5, 6, 7); 
-const int signal_pin = A0; 
+//#define DEBUG
 
 struct Control {
-  const int chan;
+  const int pin;
   int value;
   int prev_value;
   const byte effect;
 };
 
-Control crossfader = {6,0,0,0};
+Control slider_0 = {A0,0,0,7};
+Control slider_1 = {A1,0,0,8};
+Control slider_2 = {A2,0,0,9};
 
-Control h_tones_l = {0,0,0,1};
-Control m_tones_l = {1,0,0,2};
-Control l_tones_l = {2,0,0,3};
+Control knob_0 = {A3,0,0,1};
+Control knob_1 = {A4,0,0,2};
+Control knob_2 = {A5,0,0,3};
 
-Control h_tones_r = {3,0,0,4};
-Control m_tones_r = {4,0,0,5};
-Control l_tones_r = {5,0,0,6};
+Control knob_3 = {A7,0,0,4};
+Control knob_4 = {A10,0,0,5};
+Control knob_5 = {A11,0,0,6};
 
 
-Control controls[] = {crossfader,h_tones_l,m_tones_l,l_tones_l,h_tones_r,m_tones_r,l_tones_r};
+Control controls[] = {slider_0,slider_1,slider_2,knob_0,knob_1,knob_2,knob_3,knob_4,knob_5};
 const int active_controls = sizeof(controls)/sizeof(controls[0]);
 
 byte midi_value;
@@ -54,47 +54,35 @@ void controlChange(byte channel, byte control, byte value) {
 }
 
 void setup() {
-  while(!Serial);
-  Serial.begin(115200);
-  Serial.println("RDY");
-  pinMode(signal_pin, INPUT);
+  #ifdef DEBUG
+    while(!Serial);
+    Serial.begin(115200);
+    Serial.println("RDY");
+  #endif
 }
 
 void loop() {
   for (int i = 0; i < active_controls; i++) {
-    mux.channel(controls[i].chan);
+    controls[i].value = analogRead(controls[i].pin);
     
-    controls[i].value = analogRead(signal_pin);
     if ( abs(controls[i].value - controls[i].prev_value) > 9 ){
 
       midi_value = map(controls[i].value,0,1023,0,127);
       controlChange(1,controls[i].effect,midi_value);
-      
-//      Serial.print("Sent ");
-//      Serial.print(controls[i].effect);
-//      Serial.print(",");      
-//      Serial.print(controls[i].value);
-//      Serial.print(",");
-//      Serial.print(controls[i].prev_value);
-//      Serial.print(",");
-//      Serial.println( abs(controls[i].value - controls[i].prev_value) );
+      #ifdef DEBUG
+        Serial.print("Sent ");
+        Serial.print(controls[i].effect);
+        Serial.print(",");      
+        Serial.print(controls[i].value);
+        Serial.print(",");
+        Serial.print(controls[i].prev_value);
+        Serial.print(",");
+        Serial.println( abs(controls[i].value - controls[i].prev_value) );
+      #endif
       controls[i].prev_value = controls[i].value;
       
       delay(10);
       MidiUSB.flush();
     }
   }
-  /*crossfader.value = analogRead(crossfader.pin);
-  bpm.value = analogRead(bpm.pin);
-  h_tones.value = analogRead(h_tones.pin);
-  m_tones.value = analogRead(m_tones.pin);
-  l_tones.value = analogRead(l_tones.pin);
-  if ( abs(value-prev_value) > 3) {
-    prev_value = value;
-    midi_value = map(value,0,1023,0,127);
-    controlChange(1,10,midi_value);
-    delay(10);
-    MidiUSB.flush();
-  } */
-
 }
